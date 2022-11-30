74bf10df50c51da2465192ff1063c306ed3e1e73
gabrielkf <gabrielkf@gmail.com>
2022-11-30 18:03:19 -0300

    Refactor url to axios client variable

--- src/services/apiService.ts
+++ src/services/apiService.ts
@@ -5,15 +5,13 @@ import { Contact } from '../entities/Contact';
 import { HttpException } from '../entities/HttpException';
 
 const SERVER_PORT = 3030;
-const baseUrl = `http://localhost:${SERVER_PORT}/contacts`;
-
-// const httpClient = axios.create({
-//   baseURL: `http://localhost:${this.SERVER_PORT}/contacts`,
-// });
+const apiClient = axios.create({
+  baseURL: `http://localhost:${SERVER_PORT}/contacts`,
+});
 
 export async function listContacts(): Promise<Contact[]> {
   try {
-    const { data, status } = await axios.get<Contact[]>(baseUrl);
+    const { data, status } = await apiClient.get<Contact[]>('');
 
     if (status !== StatusCodes.OK) {
       throw new HttpException(getReasonPhrase(status), status);
